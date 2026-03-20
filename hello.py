0d2b3a20b21a6598960eec7baf7f22d645f74b31
aysinha23 <ayush.sinha2308@gmail.com>
2026-03-20 00:44:21 -0400

    Initial python script

---
+++ hello.py
@@ -0,0 +1,12 @@
+# 1. Variables: Storing information
+user_name = "AYUSH"
+coding_goal = "Build something cool"
+
+# 2. Logic: The 'if' statement
+if coding_goal == "Build something cool":
+    status = "on the right track"
+else:
+    status = "still exploring"
+
+# 3. Output: Printing a formatted message
+print(f"Hello {user_name}! You are {status} to {coding_goal.lower()}.")
